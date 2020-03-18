acf596452d9c4b518c2425dbe033d0808737e483
JoJoBeBop <jori.blom@metropolia.fi>
2020-03-18 12:14:17 +0200

    Cleaned up code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 const express = require('express');
-
 const cors = require("cors")
 const app = express();
 const port = 3000;
@@ -8,11 +7,9 @@ const cats = require("./routes/catRoute");
 const users = require("./routes/userRoute");
 
 
-
 app.use(cors());
 app.use("/cat", cats);
 app.use("/user", users);
-
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -9,12 +9,6 @@ router.get("/", catController.cat_list_get);
 
 router.get("/:id", catController.cat_get);
 
-/*router.get('/cat/:id?', (req, res) => {
-  console.log("cat id param: ", req.params.id);
-  res.send('From this endpoint you can requested cat whose id is: ' + req.params.id)
-});*/
-
-
 router.post('/', (req, res) => {
   res.send('From this endpoint you can ADD cats.')
 });
